f73796771c26dfb061d275af4947ebb5bf7ccd6b
Lubos Lenco <lubos.lenco@gmail.com>
2015-08-18 11:11:40 +0200

    Android progress

--- android/java/sharekore/ShareKore.java
+++ android/java/sharekore/ShareKore.java
@@ -6,14 +6,59 @@ import com.ktxsoftware.kore.KoreActivity;
 public class ShareKore {
     
 	public static void share(String subject, String body, String url, boolean attachScreenshot) {
-		System.out.println(subject);
 		Intent sendIntent = new Intent(android.content.Intent.ACTION_SEND);
 		sendIntent.setType("text/plain");
-        if(subject!=null && subject!="")	sendIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
-        if(body!=null && body!="")	sendIntent.putExtra(android.content.Intent.EXTRA_TEXT, body);
-        //if(html!=null && html!="")	sendIntent.putExtra(android.content.Intent.EXTRA_HTML_TEXT, html);
-		//if(email!=null && email!="") sendIntent.putExtra(android.content.Intent.EXTRA_EMAIL, email);
+        if (subject != null && subject != "") sendIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
+        if (body != null && body != "") sendIntent.putExtra(android.content.Intent.EXTRA_TEXT, body);
         KoreActivity.getInstance().startActivity(sendIntent);
 	}
 
+	/*public void shareit()
+    {
+        View view =  findViewById(R.id.layout);//your layout id
+        view.getRootView();
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) 
+        {
+            File picDir  = new File(Environment.getExternalStorageDirectory()+ "/myPic");
+            if (!picDir.exists())
+            {
+                picDir.mkdir();
+            }
+            view.setDrawingCacheEnabled(true);
+            view.buildDrawingCache(true);
+            Bitmap bitmap = view.getDrawingCache();
+//          Date date = new Date();
+            String fileName = "mylove" + ".jpg";
+            File picFile = new File(picDir + "/" + fileName);
+            try 
+            {
+                picFile.createNewFile();
+                FileOutputStream picOut = new FileOutputStream(picFile);
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), (int)(bitmap.getHeight()/1.2));
+                boolean saved = bitmap.compress(CompressFormat.JPEG, 100, picOut);
+                if (saved) 
+                {
+                    Toast.makeText(getApplicationContext(), "Image saved to your device Pictures "+ "directory!", Toast.LENGTH_SHORT).show();
+                } else 
+                {
+                    //Error
+                }
+                picOut.close();
+            } 
+            catch (Exception e) 
+            {
+                e.printStackTrace();
+            }
+            view.destroyDrawingCache();
+        } else {
+            //Error
+
+        }
+
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("image/jpeg");
+        sharingIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse(picFile.getAbsolutePath()));
+        startActivity(Intent.createChooser(sharingIntent, "Share via"));
+    }*/
 }
