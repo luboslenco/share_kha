8d769e3305b5146859a47d422a30d7ded55562b3
Lubos Lenco <lubos.lenco@gmail.com>
2016-01-25 09:32:42 +0100

    Update to latest Kha

--- android/sharekore/ShareKore.cpp
+++ android/sharekore/ShareKore.cpp
@@ -1,21 +1,23 @@
 #include <ShareKore.h>
 
-#include <jni.h>
-
-JNIEnv* getEnv();
+#include <Kore/Android.h>
 
 namespace ShareKore {
 
 	void share(const char* subject, const char* body, const char* url, bool attachScreenshot) {	
-        
-        jclass cls = getEnv()->FindClass("sharekore/ShareKore");
+        JNIEnv* env;
+		KoreAndroid::getActivity()->vm->AttachCurrentThread(&env, NULL);
+		
+        jclass cls = KoreAndroid::findClass(env, "sharekore.ShareKore");
 
-        jmethodID shareId = getEnv()->GetStaticMethodID(cls, "share", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Z)V");
+        jmethodID shareId = env->GetStaticMethodID(cls, "share", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Z)V");
 
-        jstring jsubject = getEnv()->NewStringUTF(subject);
-        jstring jbody = getEnv()->NewStringUTF(body);
-        jstring jurl = getEnv()->NewStringUTF(url);
+        jstring jsubject = env->NewStringUTF(subject);
+        jstring jbody = env->NewStringUTF(body);
+        jstring jurl = env->NewStringUTF(url);
 
-        getEnv()->CallStaticVoidMethod(cls, shareId, jsubject, jbody, jurl, attachScreenshot);
+        env->CallStaticVoidMethod(cls, shareId, jsubject, jbody, jurl, attachScreenshot);
+
+        KoreAndroid::getActivity()->vm->DetachCurrentThread();
     }
 }
